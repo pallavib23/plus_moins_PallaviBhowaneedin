a9dbe30376a84f198d1fd3e8cbc0a084312347d0
pallavib23 <ppallavibhowaneedin@gmail.com>
2022-07-25 23:11:16 +0400

    code avec niveau

--- plus_moins.c
+++ plus_moins.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h> //for random
 
-void plus_moins(int valeur, int choix)
+void plus_moins(int valeur, int choix, char choisi)
 {
 	if(choix>valeur)
 		printf("Le nombre est plus petit.\n");
@@ -15,12 +15,41 @@ void main()
 {
 	int valeur;
 	int choix;
+	int i=0;
+	char choisi;
+
 	valeur=rand() % 100 + 0; //random generator
 
-	do
-	{
-		printf("Enrez une valeur : ");
-		scanf("%d", &choix);
-		plus_moins(valeur,choix);
-	}while(valeur!=choix);
+	printf("choisir le mode de diificulté : \n");
+	printf("facile=f : essaies illimités\n");
+	printf("moyen=m : 25 essaies\n");
+	printf("difficle=d : 10 essaies\n");
+	printf("Votre choix : ");
+	scanf("%s", &choisi);
+
+	if(choisi=='f'){
+		do
+		{
+			printf("Entrez une valeur : ");
+			scanf("%d", &choix);
+			plus_moins(valeur,choix,choisi);
+		}while(valeur!=choix);
+	}else if(choisi=='m'){
+		do 
+		{
+			printf("Entrez une valeur : ");
+			scanf("%d", &choix);
+			plus_moins(valeur,choix,choisi);
+			i++;
+		}while(i<25);
+	}else if(choisi=='d'){
+		do
+		{
+			printf("Entrez une valeur : ");
+			scanf("%d", &choix);
+			plus_moins(valeur,choix,choisi);
+			i++;
+		}while(i<10);
+	}
+
 }
